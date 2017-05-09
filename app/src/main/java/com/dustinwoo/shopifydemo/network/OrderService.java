f57120d45eabd2b570f5bcdf91a4cb238ae1881d
Dustin Woo <hi@dustinwoo.com>
2017-05-08 23:27:47 -0400

    Create retrofit service for orders

---
+++ app/src/main/java/com/dustinwoo/shopifydemo/network/OrderService.java
@@ -0,0 +1,17 @@
+package com.dustinwoo.shopifydemo.network;
+
+import com.dustinwoo.shopifydemo.models.Order;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by dustin on 2017-05-08.
+ */
+
+public interface OrderService {
+
+    @GET("admin/orders.json")
+    Observable<Order> orderList(@Query("page") int pageNum, @Query("access_token") String token);
+}
