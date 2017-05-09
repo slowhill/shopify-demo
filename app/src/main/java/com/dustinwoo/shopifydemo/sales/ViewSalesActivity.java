af4fdb2ce09938783f29c4fb0d441c9d696769c8
Dustin Woo <hi@dustinwoo.com>
2017-05-08 23:30:51 -0400

    Creating concrete activity to display sales figures

---
+++ app/src/main/java/com/dustinwoo/shopifydemo/sales/ViewSalesActivity.java
@@ -0,0 +1,15 @@
+package com.dustinwoo.shopifydemo.sales;
+
+import com.dustinwoo.shopifydemo.R;
+import com.dustinwoo.shopifydemo.activities.BaseActivity;
+
+/**
+ * Created by dustin on 2017-05-08.
+ */
+
+public class ViewSalesActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_view_sales;
+    }
+}
